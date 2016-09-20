1ed9c4f16ba360408d60e0654e162793b141d801
Minh Van <minh.van1789@gmail.com>
2016-09-20 21:26:04 +0700

    Updated create-article gulp task

--- _configs/gulp-tasks/create-article.js
+++ _configs/gulp-tasks/create-article.js
@@ -58,10 +58,12 @@ function _getComponentFileContent(id) {
         this.id = ${id};
         this.sanitizer = sanitizer;
         this.articleService = articleService;
-
-        this.codeBlock = this.getCodeBlock('code-block.html');
       }
     ];
+
+    this.ngOnInit = function() {
+      this.codeBlock = this.getCodeBlock('code-block.html');
+    };
 
     this.getCodeBlock = function(fileName) {
       var _codeBlock = this.articleService.getCodeBlock(this.id, fileName); 
