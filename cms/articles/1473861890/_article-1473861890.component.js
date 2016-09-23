66e06af3b12936410c5cf5afb5ab3aa4415df8f5
Minh Van <minh.van1789@gmail.com>
2016-09-23 23:34:40 +0700

    Updated article 1473861890

--- cms/articles/1473861890/_article-1473861890.component.js
+++ cms/articles/1473861890/_article-1473861890.component.js
@@ -20,88 +20,90 @@ function _article1473861890Component(){
       this.id = 1473861890;
       this.sanitizer = sanitizer;
       this.articleService = articleService;
-
-      this.ngOnChanges = {
-        sourceCode: {
-          exampleComponent: {
-            name: 'example.component.js',
-            link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-on-changes/example.component.js')
-          }
-        },
-        screenCaptures: {
-          1: resourceUtils.getImg('ngOnChanges-example-1473861890.png')
-        },
-        codeBlocks: {
-          1: this.getCodeBlock('ng-on-changes.html')
-        }
-      };
-
-      this.ngOnInit = {
-        sourceCode: {
-          childComponent: {
-            name: 'child.component.js',
-            link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-on-init/child.component.js')
-          }
-        },
-        screenCaptures: {
-          1: resourceUtils.getImg('ngOnInit-example-1473861890.png')
-        },
-        codeBlocks: {
-          1: this.getCodeBlock('ng-on-init.html')
-        }
-      };
-
-      this.ngDoCheck = {
-        sourceCode: {
-          childComponent: {
-            name: 'child.component.js',
-            link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-do-check/child.component.js')
-          }
-        },
-        screenCaptures: {
-          1: resourceUtils.getImg('ngDoCheck-example-1473861890.png')
-        },
-        codeBlocks: {
-          1: this.getCodeBlock('ng-do-check.html')
-        }
-      };
-
-      this.ngAfterContent = {
-        sourceCode: {
-          childComponent: {
-            name: 'child.component.js',
-            link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-after-content/child.component.js')
-          },
-          exampleComponent: {
-            name: 'example.component.js',
-            link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-after-content/example.component.js')
-          }
-        },
-        screenCaptures: {
-          1: resourceUtils.getImg('ngAfterContent-example-1473861890.png')
-        },
-        codeBlocks: {
-          1: this.getCodeBlock('ng-after-content-1.html'),
-          2: this.getCodeBlock('ng-after-content-2.html')
-        }
-      };
-
-      this.ngAfterView = {
-        sourceCode: {
-          exampleComponent: {
-            name: 'example.component.js',
-            link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-after-view/example.component.js')
-          }
-        },
-        screenCaptures: {
-          1: resourceUtils.getImg('ngAfterView-example-1473861890.png')
-        },
-        codeBlocks: {
-          1: this.getCodeBlock('ng-after-view.html')
-        }
-      };
     }
   ];
+
+  this.ngOnInit = function() {
+    this.ngOnChanges = {
+      sourceCode: {
+        exampleComponent: {
+          name: 'example.component.js',
+          link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-on-changes/example.component.js')
+        }
+      },
+      screenCaptures: {
+        1: resourceUtils.getImg('ngOnChanges-example-1473861890.png')
+      },
+      codeBlocks: {
+        1: this.getCodeBlock('ng-on-changes.html')
+      }
+    };
+
+    this.ngOnInit = {
+      sourceCode: {
+        childComponent: {
+          name: 'child.component.js',
+          link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-on-init/child.component.js')
+        }
+      },
+      screenCaptures: {
+        1: resourceUtils.getImg('ngOnInit-example-1473861890.png')
+      },
+      codeBlocks: {
+        1: this.getCodeBlock('ng-on-init.html')
+      }
+    };
+
+    this.ngDoCheck = {
+      sourceCode: {
+        childComponent: {
+          name: 'child.component.js',
+          link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-do-check/child.component.js')
+        }
+      },
+      screenCaptures: {
+        1: resourceUtils.getImg('ngDoCheck-example-1473861890.png')
+      },
+      codeBlocks: {
+        1: this.getCodeBlock('ng-do-check.html')
+      }
+    };
+
+    this.ngAfterContent = {
+      sourceCode: {
+        childComponent: {
+          name: 'child.component.js',
+          link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-after-content/child.component.js')
+        },
+        exampleComponent: {
+          name: 'example.component.js',
+          link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-after-content/example.component.js')
+        }
+      },
+      screenCaptures: {
+        1: resourceUtils.getImg('ngAfterContent-example-1473861890.png')
+      },
+      codeBlocks: {
+        1: this.getCodeBlock('ng-after-content-1.html'),
+        2: this.getCodeBlock('ng-after-content-2.html')
+      }
+    };
+
+    this.ngAfterView = {
+      sourceCode: {
+        exampleComponent: {
+          name: 'example.component.js',
+          link: resourceUtils.getGithubArticleFileLink(this.id, 'ng-after-view/example.component.js')
+        }
+      },
+      screenCaptures: {
+        1: resourceUtils.getImg('ngAfterView-example-1473861890.png')
+      },
+      codeBlocks: {
+        1: this.getCodeBlock('ng-after-view.html')
+      }
+    };
+  };
 
   this.getCodeBlock = function(fileName) {
     var _codeBlock = this.articleService.getCodeBlock(this.id, fileName); 
