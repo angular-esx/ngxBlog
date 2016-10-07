8e3eef8bfc71bb8cd6b35fa5018c874203b91027
Minh Van <minh.van1789@gmail.com>
2016-10-08 00:21:51 +0700

    Changed to use syntax ngCore.Component().Class()

--- cms/articles/1474905680/_article-1474905680.component.js
+++ cms/articles/1474905680/_article-1474905680.component.js
@@ -107,4 +107,86 @@ export var article1474905680Component = ngCore.Component({
     '[class.xblog-article-1474905680]': 'true'
   }
 })
-.Class(new _article1474905680Component());
+.Class({
+  constructor: [
+    DomSanitizationService,
+    cmsArticleService,
+    tableContentService,
+
+    function (sanitizer, articleService, tableContentService){
+      this.id = 1474905680;
+      this.sanitizer = sanitizer;
+      this.articleService = articleService;
+      this.tableContentService = tableContentService;
+    }
+  ],
+
+  ngOnInit: function() {
+    this.pipeArticleLink = '#'
+
+    this.tableContents = this.tableContentService
+    .getBuilder()
+    .addHeadings([
+      { id: 'interpolation', name: 'Interpolation' },
+      { id: 'template-expressions', name: 'Template expressions' },
+      { id: 'template-statements', name: 'Template statements' },
+      { id: 'template-reference-variables', name: 'Template reference variables' },
+      { id: 'template-expression-operators', name: 'Template expression operators' }
+    ])
+    .addSubHeadings([
+      { headingId: 'template-expressions', id: 'expression-context', name: 'Expression context' },
+      { headingId: 'template-expressions', id: 'expression-guidelines', name: 'Expression guidelines' },
+      { headingId: 'template-statements', id: 'statement-context', name: 'Statement context' },
+      { headingId: 'template-statements', id: 'statement-guidelines', name: 'Statement guidelines' },
+      { headingId: 'template-expression-operators', id: 'the-pipe-operator', name: 'The pipe operator ( | )' },
+      { headingId: 'template-expression-operators', id: 'the-safe-navigation-operator', name: 'The safe navigation operator ( ?. )' }
+    ])
+    .build();
+
+    this.interpolation = {
+      codeBlocks: {
+        1: this.getCodeBlock('interpolation.html'),
+      }
+    }; 
+
+    this.templateRefVariable = {
+      sourceCode: {
+        name: 'example.component.js',
+        link: resourceUtils.getGithubArticleFileLink(this.id, 'template-ref-variable/example.component.js')
+      },
+      codeBlocks: {
+        1: this.getCodeBlock('template-ref-variable.html'),
+      },
+      screenCaptures: {
+        1: resourceUtils.getImg('templateRefVariable-example-1474905680.png')
+      }
+    }; 
+
+    this.pipeOperator = {
+      codeBlocks: {
+        1: this.getCodeBlock('pipe-operator-1.html'),
+        2: this.getCodeBlock('pipe-operator-2.html'),
+        3: this.getCodeBlock('pipe-operator-3.html'),
+      }
+    }; 
+
+    this.safeNavOperator = {
+      sourceCode: {
+        name: 'example.component.js',
+        link: resourceUtils.getGithubArticleFileLink(this.id, 'safe-nav-operator/example.component.js')
+      },
+      codeBlocks: {
+        1: this.getCodeBlock('safe-nav-operator.html'),
+      }
+    };
+  },
+
+  getCodeBlock: function(fileName, lang) {
+    var _langs = lang ? [ lang ] : ['javascript', 'html', 'css'];
+
+    var _codeBlock = this.articleService.getCodeBlock(this.id, fileName); 
+    _codeBlock = highlight.highlightAuto(_codeBlock, _langs).value;
+
+    return this.sanitizer.bypassSecurityTrustHtml(_codeBlock);
+  }
+});
