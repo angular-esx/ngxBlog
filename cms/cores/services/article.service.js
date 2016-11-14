7bbe6cf2cec80c31801ee163689af1c5204117f5
Minh Van <minh.van1789@gmail.com>
2016-11-14 21:55:47 +0700

    Changed to use template literal for article's code block

--- cms/cores/services/article.service.js
+++
@@ -1,9 +0,0 @@
-import { Class } from '@angular/core';
-
-export var cmsArticleService = Class({
-  constructor: function cmsArticleService(){},
-
-  getCodeBlock: function(id, fileName){
-    return require('../../articles/' + id + '/code-blocks/' + fileName);
-  }
-});
